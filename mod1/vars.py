f99fba8b1b350e13ca54fbcaa790a777dd8bee81
ctwobosius <ctwong958@berkeley.edu>
2020-06-09 11:34:20 -0700

    Beginning vars and math tutorial

---
+++ mod1/vars.py
@@ -0,0 +1,42 @@
+# <- This is a comment. Python ignores anything on this commented line!
+
+"A String" # basically words/sentences
+
+old_meme = "Harambe"
+# variable name = value
+
+print(old_meme)
+# What will this print out?
+
+print("old_meme")
+# What about this?
+
+# MATH TIME! Don't worry it's simple
+
+314 # integer (whole number, no decimal)
+
+pi_imposter = 314
+
+print(pi_imposter + 1)
+# What will this be?
+
+# basic maths
+print(2 + 2)
+print(5 - 3)
+print(4 * 2)
+print(5 / 2) # something interesting happens here
+
+1.5 # float (decimal(s) included)
+
+print(4 / 2)
+# Will this be 2 or 2.0? Int or float?
+
+print(int(4 / 2))
+print(int(5 / 2)) # rounds down
+print(5 // 2) # floor division (cutting off the decimal, pretty much the same as int())
+
+# print(1 / 0) # division by 0? What does this do?
+
+print(1 % 3) # modulus, gives the remainder of 1 / 3
+
+print(2 ** 3) # exponentiation, 2 to the power of 3
