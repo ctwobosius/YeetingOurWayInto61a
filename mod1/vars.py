b3664304f74718c8a06dc3e6d428f6bfe6b6ecbb
Calvin Wong <ctwong958@berkeley.edu>
2020-06-10 10:16:19 -0700

    Wrap up module 1

--- mod1/vars.py
+++ mod1/vars.py
@@ -31,12 +31,40 @@ print(5 / 2) # something interesting happens here
 print(4 / 2)
 # Will this be 2 or 2.0? Int or float?
 
-print(int(4 / 2))
-print(int(5 / 2)) # rounds down
-print(5 // 2) # floor division (cutting off the decimal, pretty much the same as int())
+print(5 // 2) # floor division (cutting off the decimal)
+print(4 // 2) # What will this be?
 
-# print(1 / 0) # division by 0? What does this do?
+# print(1 / 0) 
+# division by 0? Is this possible?
 
 print(1 % 3) # modulus, gives the remainder of 1 / 3
 
 print(2 ** 3) # exponentiation, 2 to the power of 3
+
+print(int(5 / 2)) # rounds down
+print(int(4 / 2))
+
+print("Join" + " dis" + " together~") # string concatenation
+
+print(str(234)) # Convert to string
+# What will this look like?
+
+#print("String " + 18) 
+# What will this do?
+
+print("The prime number " + str(13 + 4))
+# What will this be?
+
+print("Repeat " * 3)
+
+x = 323
+# What if we want to increase x by 3? We basically want x to be x + 3.
+
+x = x + 3
+print(x)
+
+y = 323
+y += 3
+
+print(y)
+# what do you think y is?
