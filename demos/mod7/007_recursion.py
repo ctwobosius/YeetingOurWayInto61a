26e92363e5116ffe5dda098c386419a999ef06ac
ctwobosius <ctwong958@berkeley.edu>
2020-06-19 22:52:13 +0200

    Setup mod 7 recursion

--- demos/mod7/007_recursion.py
+++ demos/mod7/007_recursion.py
@@ -10,31 +10,29 @@ def dream_within_a(dream):
 #         return 0
 #     else:
 #         return ____
-
-
+#
+#
 # print(sum_from_zero_to(5))
 
 
-""" 100 factorial (written as 100!) is 100 * 99 * 98 * ... * 2 * 1.
-0! is 1.
-Could use for loop; but there is another.
-"""
-def factorial(n):
-    if n <= ____:
-        return ____
-    return factorial(____) * ____
-
-
-
-
-
-def factorial_iter(n):
-    if n <= ____:           # to deal with negative n
-        return 1
-    product = n             # start product at n
-    for i in range(____, ____):   # for each number from 1 all the way to n-1,
-        product *= i        # multiply it with our product
-    return ____
-
-print(factorial(5))
-print(factorial_iter(5))
+# """ 100 factorial (written as 100!) is 100 * 99 * 98 * ... * 2 * 1.
+# 0! is 1.
+# Could use for loop; but there is another.
+# """
+# def factorial(n):
+#     if n <= ____:
+#         return ____
+#     return factorial(____) * ____
+#
+#
+#
+# def factorial_iter(n):
+#     if n <= ____:           # to deal with negative n
+#         return 1
+#     product = n             # start product at n
+#     for i in range(____, ____):   # for each number from 1 all the way to n-1,
+#         product *= i        # multiply it with our product
+#     return ____
+#
+# print(factorial(5))
+# print(factorial_iter(5))
