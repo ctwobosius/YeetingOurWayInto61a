992aa88511c958cd4bda0ef624719c89aee7fc69
ctwobosius <ctwong958@berkeley.edu>
2020-07-18 20:22:08 -0700

    FInish iterators/generators

--- demos/020_iterators_generators/it_gens.py
+++ demos/020_iterators_generators/it_gens.py
@@ -4,15 +4,74 @@
 
 """
 
+lst = [1, 2, 3]
+x = iter(lst)
+next(x)
+1
+next(x)
+2
+next(x)
+3
+# next(x)
+# StopIteration Error
+
+y = iter(lst)
+next(y)
+1
+x
+
+
+y[0]
+
+
+
 pizza = ["r", "i", "p", "p", "e", "r", "o", "n", "i"]
+# can you call next on iterables?
 print(next(pizza))
-weird = iter(pizza)
+# iterables vs iterators
+error
+
+weird = iter(pizza) # iter creates iterator
 print(pizza[0])
+# r
 print(weird[0])
+# error
 
 print(next(weird))
-next(weird)
-next(weird)
+
+lst
+dictionary = {0: "jesus"}
+string = "st"
+"string"[0]
+>>> dictionary[0]
+"jesus"
+
+dictionary.values()
+for key in dict:
+    if dict[key] == value:
+        return key
+
+iter(dictionary.values())
+iter(dictionary.keys())
+"fire"
+1
+d = {0: "water", 1: "fire", 2: "earth"}
+
+# v = {"water", "fire", "earth"}
+v = iter(d.values())
+
+# k = {0, 1, 2}
+k = iter(d.keys())
+
+for value in v:
+    naming_is_hard = next(k)
+    if value == "fire":
+        print(naming_is_hard)
+
+
+print(next(weird)) r
+next(weird) i
+next(weird) p
 
 print([pepper for pepper in weird])
 print([pepper for pepper in pizza])
---
+++ demos/020_iterators_generators/lol.py
@@ -0,0 +1,130 @@
+d = {0: "water", 1: "fire", 2: "earth", 3: "nitrogen"}
+
+# v = {"water", "fire", "earth"}
+v = iter(d.values())
+
+# k = {0, 1, 2}
+k = iter(d.keys())
+
+for value in v:
+    naming_is_hard = next(k)
+    if value == "nitrogen":
+        print(naming_is_hard)
+
+
+def g():
+    return 1
+    return 2
+
+
+def gen():
+    yield 1
+    yield 2
+    yield 3
+
+
+
+x = iter([23])
+x = gen()
+next(x) # 1
+next(x) # 2
+next(x) # 3
+y = gen()
+z = gen()
+next(y)
+next(z)
+next(y)
+
+
+def big_name():
+    lst = [1, 2, 3]
+    for i in lst:
+        yield i
+
+def big_name():
+    lst = [1, 2, 3]
+    yield from lst
+
+x = big_name()
+next(x)
+1
+next(x)
+2
+next(x)
+3
+next(x)
+StopIteration error
+
+
+# Infinite generation: Create a function that yields a cycle of LST:
+# lst = [1, 2, 3]
+# x = gen(lst)
+# next(x)
+# 1
+# next(x)
+# 2
+# next(x)
+# 3
+# next(x)
+# 1
+# next(x)
+# 2
+# next(x)
+# 3
+# next(x)
+# 1
+# next(x)
+# 2
+# ...
+def gen(lst):
+    i = ____
+    while ____:
+        if _____:
+
+            yield lst[i] # lst[0]
+            _____
+        else:
+
+            yield lst[____]
+            ____
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# solution:
+# def gen(lst):
+#     i = 0
+#     while True:
+#         if i < len(lst):
+#
+#             yield lst[i] # lst[0]
+#             i += 1
+#         else: # if hit end
+#
+#             yield lst[0] # lst[0]
+#             i = 1
