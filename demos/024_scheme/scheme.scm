2323a4d98bd714423b5f61b55b48e5c7f1090f9c
ctwobosius <ctwong958@berkeley.edu>
2020-07-26 18:58:13 -0400

    more Scheme

--- demos/024_scheme/scheme.scm
+++ demos/024_scheme/scheme.scm
@@ -1,6 +1,7 @@
 
-((((((+ 3 3))))))
+; Scheme: where spacing don't matter'd and parentheses are splatter'd
 ; rainbow-delimiters or bracket-colorizer
+((((((+ 3 3))))))
 
 ; Numbers
 1, 2, 333
@@ -17,4 +18,15 @@
 (+ 3 3)
 ; after evaluating operator, then operands, apply operator to operands
 
+; Control
 (if <predicate> <if-true> <if-false>)
+
+(cond
+        (<condition> <return>)
+        ((> x 0) 'positive)
+        ((< x 0) 'negative)
+        (else 'zero))       ; optional else
+
+; Defining variables and functions
+(define <name> <expression>)
+(define (<name> <param1> <param2> ...) <body>)
